be3f4df5037e25b7715ccbe415c74724ceacc225
karina <kmalina946@gmail.com>
2023-10-12 23:20:16 +0300

    editing lesson 2

--- homework_3.py
+++ homework_3.py
@@ -4,7 +4,7 @@
 #r = int (input('Яку операцію Ви хочете виконати? \n 1 Додавання \n 2 Віднімання \n 3 Множення \n 4 Ділення'))
 
 #if r == 1 :
-    result = n_1 + n_2
+    #result = n_1 + n_2
 
 #print('Результат == result')
 
@@ -14,5 +14,10 @@
 
 
 
-inp = input('Welcome, please enter a number ')
+inp = float (input('Welcome, please enter a number '))
 print('You entered: ' , inp)
+
+number_1 = float (input('Enter first number: '))
+op = input ('Enter operator (+,-,*,/,^): ')
+number_2 = float (input('Enter second number: '))
+print(number_1,op,number_2)
