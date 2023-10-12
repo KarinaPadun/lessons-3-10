8242bf3fbc70684b6bc6272a9bb6caa49aeb73ee
karina <kmalina946@gmail.com>
2023-10-12 23:13:22 +0300

    editing lesson 2

--- homework_3.py
+++ homework_3.py
@@ -1,9 +1,18 @@
-n_1 = int (input('Введіть число 1:'))
-n_2 = int(input('Введіть число 2:'))
+#n_1 = int (input('Введіть число 1:'))
+#n_2 = int(input('Введіть число 2:'))
 
-r = int (input('Яку операцію Ви хочете виконати? \n 1 Додавання \n 2 Віднімання \n 3 Множення \n 4 Ділення'))
+#r = int (input('Яку операцію Ви хочете виконати? \n 1 Додавання \n 2 Віднімання \n 3 Множення \n 4 Ділення'))
 
-if r == 1 :
+#if r == 1 :
     result = n_1 + n_2
 
-print('Результат == result')
+#print('Результат == result')
+
+
+
+
+
+
+
+inp = input('Welcome, please enter a number ')
+print('You entered: ' , inp)
