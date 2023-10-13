6b559500ffaf16ed370131b21b014563ddc1a18c
karina <kmalina946@gmail.com>
2023-10-13 18:43:31 +0300

    editing lesson 2

--- homework_3.py
+++ homework_3.py
@@ -1,25 +1,28 @@
-def calculate(number_1,number_2,op):
-    if op == '+':
-        result = number_1+number_2
-    elif op == '-':
-        result = number_1-number_2
-    elif op == '*':
-        result = number_1*number_2
-    elif op == '/':
-        if number_2 != 0:
-            result = number_1 / number_2
-        else:
-            return "Error: division by zero"
-    elif op == '^':
-        result = number_1**number_2
-    else:
-        return "Error: invalid statement"
+def calculate(number_1 , number_2 , op):
 
+  if op == '+':
+        result= number_1 + number_2
+  elif op == '-':
+        result= number_1 - number_2
+  elif op == '*':
+        result =number_1 * number_2
+  elif op == '/':
+    if number_2 != 0:
+       result = number_1 / number_2
+    else:
+       return "Error: division by zero"
+  elif op == '**':
+        result = number_1 ** number_2
+  else:
+       return "Error: invalid statement"
 
 
 number_1 = float(input('Enter first number: '))
-op = input('Enter operator (+,-,*,/,^): ')
+
+op = input('Enter operator (+,-,*,/,**): ')
+
 number_2 = float(input('Enter second number: '))
+
 print(number_1,op,number_2)
 result = calculate(number_1,number_2,op)
 print('=',result)
