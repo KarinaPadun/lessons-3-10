2bb74f61932df9d5648585f888a8ef2e0f576bdd
karina <kmalina946@gmail.com>
2023-10-13 14:35:09 +0300

    editing lesson 2

--- homework_3.py
+++ homework_3.py
@@ -27,7 +27,7 @@ def calculate(number_1,number_2,op):
         result = number_1/number_2
     elif op == '^':
         result = number_1**number_2
-'return result'
+    return result
 
 
 number_1 = float(input('Enter first number: '))
@@ -35,4 +35,4 @@ op = input ('Enter operator (+,-,*,/,^): ')
 number_2 = float(input('Enter second number: '))
 print(number_1,op,number_2)
 result = calculate(number_1,number_2,op)
-print('=','result')
+print('=',result)
