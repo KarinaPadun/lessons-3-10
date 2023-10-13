6444c8878394d6c45ecd89b99c1264d6a836cf5c
karina <kmalina946@gmail.com>
2023-10-13 14:33:06 +0300

    editing lesson 2

--- homework_3.py
+++ homework_3.py
@@ -13,12 +13,26 @@
 
 
 
+inp = float(input('Welcome, please enter a number : '))
+print('You entered: ' ,inp)
+
+def calculate(number_1,number_2,op):
+    if op == '+':
+        result = number_1+number_2
+    elif op == '-':
+        result = number_1-number_2
+    elif op == '*':
+        result = number_1*number_2
+    elif op == '/':
+        result = number_1/number_2
+    elif op == '^':
+        result = number_1**number_2
+'return result'
 
 
-inp = float (input('Welcome, please enter '))
-print('You entered: ' , inp)
-
-number_1 = float (input('Enter first number: '))
+number_1 = float(input('Enter first number: '))
 op = input ('Enter operator (+,-,*,/,^): ')
-number_2 = float (input('Enter second number: '))
+number_2 = float(input('Enter second number: '))
 print(number_1,op,number_2)
+result = calculate(number_1,number_2,op)
+print('=','result')
