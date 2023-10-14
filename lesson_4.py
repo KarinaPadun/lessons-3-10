48d74845905cd86212be6ef4707edcab4f1af563
karina <kmalina946@gmail.com>
2023-10-14 18:33:10 +0300

    editing lesson 2

--- lesson_4.py
+++ lesson_4.py
@@ -26,3 +26,26 @@
 # if (value % 2) == 0 and (value % 5) == 0:
 #     print(value)
 # if not (value % 2) and not (value % 5) aбо if not (value % 2 and value % 5):
+
+
+#logged = True
+#
+# if not logged: (сценарій що більше- на початок)
+#     print("Please logged in")
+# else:
+#     print("Welcome")
+
+
+############# ternary operator ############
+
+
+# a = 2
+# b = 330
+#
+# # print("A") if a > b else print("B")
+#
+# result = True if a > b else False
+#
+# print(result)
+
+############# while (True, flag, value) ############
