a6d651d8e73f178d8c2ae7c5c0611fc823efbabb
karina <kmalina946@gmail.com>
2023-10-14 18:28:41 +0300

    editing lesson 2

--- lesson_4.py
+++ lesson_4.py
@@ -1,5 +1,28 @@
+# 3 stages of programming (створити програму яка працює, оптимізація , переклад частин на іншу мову, щоб працювало швидше)
 
-value_str="ururyhr"
-index=4
+# if statement not
+# ternary operator
+# while (True, flag, value)
+# break, continue
+# while else
+# str methods
 
-print(value_str[::-2])
+# value = "ffff"
+
+# if (value == "+") or (value == "-"):
+#     print("!!!!!")
+# else:
+#     print("????")
+
+
+
+########### if statement not  ############
+
+# if not value: - value is not True
+#     print("ssss")
+
+#value = 10
+
+# if (value % 2) == 0 and (value % 5) == 0:
+#     print(value)
+# if not (value % 2) and not (value % 5) aбо if not (value % 2 and value % 5):
