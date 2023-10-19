25b2470beda267c6352571dce281b7529e0f8228
karina <kmalina946@gmail.com>
2023-10-19 11:34:51 +0300

    editing lesson 2

---
+++ lesson_5.py
@@ -0,0 +1,11 @@
+# debage
+# for (range, enumerate)
+# str methods (i)
+# salpha ,rfind ,replase , startwith
+
+# debage - можна поставити точки, є своя консоль , показує як відпрацьовує код по строчкам чи повність
+# step over - від точки до точки
+# step into - буде чіпляти всі функції (вбудовані і не вбудовані) , які були викликані
+# step into my code - лише по стрічках на які натиснемо
+
+
