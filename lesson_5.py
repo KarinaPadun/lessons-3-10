6d5e7f1ea0761856d1d15a53b8506c59b3309d0d
karina <kmalina946@gmail.com>
2023-10-19 11:42:49 +0300

    editing lesson 2

--- lesson_5.py
+++ lesson_5.py
@@ -2,6 +2,7 @@
 # for (range, enumerate)
 # str methods (i)
 # salpha ,rfind ,replase , startwith
+##########################################
 
 # debage - можна поставити точки, є своя консоль , показує як відпрацьовує код по строчкам чи повність
 # step over - від точки до точки
@@ -9,3 +10,12 @@
 # step into my code - лише по стрічках на які натиснемо
 
 
+############## for (range, enumerate)################
+value_str = 'Hello'
+index = 0
+# while index < len(value_str):
+# print(value_str[index])
+# index += 1
+
+for letter in value_str:
+    print(letter)
