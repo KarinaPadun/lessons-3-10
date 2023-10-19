ec2ba196844b3b441d291ce80652a9bdff29c0a1
karina <kmalina946@gmail.com>
2023-10-19 15:16:47 +0300

    editing lesson 2

--- lesson_5.py
+++ lesson_5.py
@@ -27,8 +27,38 @@ for letter in value_str:
 # там де можна використати for , 100% можна і while
 # for не потребує умови , працює з індексованими об'єктами
 
-range(10)  # повертає об'єкт , видача від 0 до 10(виключно)
+#range(10)  # повертає об'єкт , видача від 0 до 10(виключно)
+# range(5, 10)  # від 5 до 9 включно
+# range(5, 10, 2)  # від 5 до 9 включно , кожний вдругий символ
+
 #початок кінецькрок
 for index in value_str:
     #print(letter)
     print(index)
+
+for index in range(len(value_str)):
+    print(index)
+
+for index in range(len(value_str)):
+    print(index, value_str[index]) # індекс , що лежить в цьому індексі
+
+################## enumerate #############
+
+for index, letter in enumerate(value_str):
+    print(index, letter)
+
+
+for value in enumerate(value_str):
+    print(value) # кортеж
+    # print(value [0], value[1]) #розкладає без дужок
+
+
+for i in range(10): # k j виключення скороченних назв
+    print(i)
+
+
+#########################################################
+value_str = 'hello1234'
+
+for letter in value_str
+    print(letter)
