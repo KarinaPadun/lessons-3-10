be0a9b7c690af8a1c628f31951092233b85fdbcd
karina <kmalina946@gmail.com>
2023-10-20 12:30:31 +0300

    editing lesson 2

--- homework_5.py
+++ homework_5.py
@@ -26,22 +26,45 @@ for i in range(5):
     print(value_str[i])
 #d виведіть весь рядок, крім двох останніх символів.
 value_str = 'rtyuiocvb'
-result_str = value_str[:-2]
-print(result_str)
+result = value_str[:-2]
+print(result)
+#d2. виведіть весь рядок, крім двох останніх символів.
+#value_str = 'rtyuiocvb'
 
-#rfind
-#string[::-1]
+#for i in range(len(value_str) - 2):
+    #print(value_str[i], end='')
+# e. виведіть усі символи з парними індексами (вважаючи, що індексація починається з 0, тому символи виводяться починаючи)
 
-#value_str = 'rtyuio'
+value_str_1 = 'hgfd3t'
+print(value_str_1[::2])
 
- #for value_str range(0,6):
-  #  print(value_str) string[ : 5 ]
+# e2. виведіть усі символи з парними індексами (вважаючи, що індексація починається з 0, тому символи виводяться починаючи)
+value_str_1 = 'hgfd'
+for i in range(0, len(value_str_1), 2):
+    print(value_str_1[i])
+# f. виведіть усі символи з непарними індексами, тобто, починаючи з другого символу рядка.
+value_str_1 = 'hgfd3t'
+print(value_str_1[1::2])
+# f2. виведіть усі символи з непарними індексами, тобто, починаючи з другого символу рядка.
+value_str_1 = 'hgfd'
+for i in range(1, len(value_str_1), 2):
+    print(value_str_1[i])
+# g. виведіть усі символи у зворотному порядку.
+value_str_2 = 'Teyr'
+print(value_str_2[::-1])
+
+# g2. виведіть усі символи у зворотному порядку.
+value_str_3 = 'Yuio'
+for i in range(len(value_str_3)-1,-1,-1):
+    print(value_str_3[i])
+
+
 
 
 
 #i. виведіть довжину цього рядка.
 
-value_str = 'yr6uiooo'
+#value_str = 'yr6uiooo'
 
-if len(value_str):
-    print(len(value_str))
+#if len(value_str):
+  #  print(len(value_str))
