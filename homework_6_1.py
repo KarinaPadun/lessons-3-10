c9ab7aa8f3cd09dcfb2a15f78ce88782b3356cb2
karina <kmalina946@gmail.com>
2023-10-21 20:01:03 +0300

    editing lesson 2

--- homework_6_1.py
+++ homework_6_1.py
@@ -42,8 +42,33 @@ for char in my_str:
 print(my_list)
 ############################
 my_str = 'I am 12 years '
+my_list = []
 my_str = my_str.lower()
 
 my_list = list(set(my_str))
 
 print(my_list)
+#####################
+#Дано рядок my_str та порожній список my_list. Заповнити my_list символами з my_str,
+#які стоять на парних місцях у рядку (вважаємо з 0)
+#Наприклад: my_str = "bla BLA car"
+           #Виведення на екран: ["b", "a", "B", "A", "c", "r"]
+my_str = 'hi! how are you'
+my_list = []
+
+for i in range(0, len(my_str), 2):
+    my_list.append(my_str[i])
+
+print(my_list)
+####################################
+my_str = 'hi! how are you'
+my_list = list(my_str[::2])
+
+print(my_list)
+
+#########################################
+#Дано рядок my_str, список str_index цілих чисел в діапазоні від 0 до довжини рядка мінус 1, пустий список my_list.
+#Заповнити my_list символами my_str, які стоять на місцях з індексами із str_index
+my_str = 'eiri eir fvvf g '
+str_index= [1, 3, 5, 6, 8, 12]
+my_list = []
