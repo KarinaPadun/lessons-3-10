f5bcbf42ba57d655b6b4264ae6d898e0530ed010
karina <kmalina946@gmail.com>
2023-10-21 19:50:43 +0300

    editing lesson 2

--- homework_6_1.py
+++ homework_6_1.py
@@ -1,3 +1,7 @@
+#Ви маєте змінну my_str, тип - str. Надрукувати ЧИСЛО скільки РІЗНИХ символів зустрічається в my_str.
+#Велика та маленька літера вважається одним символом. Пробіл, коми і т.д. вважаємо також як символи.
+#Наприклад: my_str="bla BLA car"
+
 my_str = "Twelve, minutes"
 unique_chars = set(my_str.lower())  # Перетворення усіх літер в нижній регістр та використання множини для визначення унікальних символів
 count_unique_chars = len(unique_chars)  # Підрахунок кількості унікальних символів
@@ -18,3 +22,28 @@ for char in my_str:
 count_unique_chars = len(unique_chars)
 
 print(count_unique_chars)
+
+
+
+#Ви маєте змінну my_str, та порожній список my_list. Заповнити my_list УНІКАЛЬНИМИ символами з my_str.
+#Велика та маленька літера вважається одним символом. Пробіл, коми і т.д. вважаємо також як символи.
+#Наприклад: my_str = "bla BLA car"
+#           Виведення на екран: ["b", "l", "a", " ", "c", "r"]
+
+my_str = 'I am 12 years old '
+my_list = []
+
+my_str = my_str.lower()
+
+for char in my_str:
+    if char not in my_list:
+        my_list.append(char)
+
+print(my_list)
+############################
+my_str = 'I am 12 years '
+my_str = my_str.lower()
+
+my_list = list(set(my_str))
+
+print(my_list)
