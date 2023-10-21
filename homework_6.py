74760b4dcd5f7f3303f72e45507db931d0f2348c
karina <kmalina946@gmail.com>
2023-10-21 16:01:09 +0300

    editing lesson 2

--- homework_6.py
+++ homework_6.py
@@ -5,3 +5,10 @@ for i in my_list:
     if i > 100:
      print(i)
 
+# 1.2 варіант
+my_list = [1, 99, 102, 105, 2001, 999]
+
+for i in range(len(my_list)):
+    if my_list[i] > 100:
+        print(my_list[i])
+
