0df3993770eea9aefe993a5d7fcc99e9f809800b
karina <kmalina946@gmail.com>
2023-10-27 12:49:03 +0300

    editing lesson 2

--- homework_7.py
+++ homework_7.py
@@ -81,6 +81,10 @@ my_list_1 = [-3, 3, -455, 20, 114]
 my_list_2 = [3, 0, 9, 4, 8, 11]
 my_result = []
 
+# my_result = [my_list_1[i] for i in range(0, len(my_list_1), 2)] + [my_list_2[i] for i in range(0, len(my_list_2), 2)]
+
+# my_result = [i for i in my_list_1[::2]] + [i for i in my_list_2[::2]]
+
 for i in my_list_1:
     if my_list_1.index(i) % 2 == 0:  #
         my_result.append(i)
--- lesson_1.py
+++ lesson_1.py
@@ -1,8 +1,14 @@
 # print("hello world")
 
-
 my_list_1 = [-3, 3, -455, 20, 114]
 my_list_2 = [3, 0, 9, 4, 8, 11]
-my_result = [i for i in my_list_1[::2]]
-my_result.extend(my_list_2[::2])
+
+# my_result = [my_list_1[i] for i in range(0, len(my_list_1), 2)] + [my_list_2[i] for i in range(0, len(my_list_2), 2)]
+
+# my_result = [i for i in my_list_1[::2]] + [i for i in my_list_2[::2]]
+
 print(my_result)
+
+
+
+
