bed04cc555713e5dd387696eb07b2d6083d45a76
karina <kmalina946@gmail.com>
2023-10-27 13:11:31 +0300

    editing lesson 2

--- lesson_1.py
+++ lesson_1.py
@@ -1,11 +1,3 @@
 # print("hello world")
 
 
-my_str = 'this is my home'
-my_list = [i for i in my_str if my_str.count(i) == 1]
-
-
-print(my_list)
-
-
-
--- lessons_7_1.py
+++ lessons_7_1.py
@@ -2,7 +2,17 @@
 # sets
 # sets methods(union(), difference(), intersection())
 
+# list = []
+# list += (my_str [::2])   # щоб лист не перестворювався , поставити +=
+
+
+
+
+
+
+
 ################# list comprehension
+
 
 value_str = [1, 2, 3, 4]
 
