9b597661a86d853ef78ac9d62c04a3912445fdbd
karina <kmalina946@gmail.com>
2023-10-27 14:10:59 +0300

    editing lesson 2

--- lessons_7_1.py
+++ lessons_7_1.py
@@ -4,15 +4,19 @@
 
 # list = []
 # list += (my_str [::2])   # щоб лист не перестворювався , поставити +=
-
-
-
-
-
-
-
 ################# list comprehension
-
-
 value_str = [1, 2, 3, 4]
+# some_list = []
+# for i in value_str:
+#     if not (i**2) % 2:
+#         some_list.append(i**2)
 
+# some_list = [i for i in value_str]
+# some_list = [i**2 for i in value_str]
+# some_list = [i**2 for i in value_str if not (i**2) % 2]
+
+# print(some_list)
+
+# list comprehension - генератор списків . генерує списки та кладе туди елементи
+
+# sets - множина
