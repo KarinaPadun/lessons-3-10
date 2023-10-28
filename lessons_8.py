0603367d86f5853071fcbea66c5aa77c034108b8
karina <kmalina946@gmail.com>
2023-10-28 11:48:15 +0300

    editing lesson 2

---
+++ lessons_8.py
@@ -0,0 +1,25 @@
+# розпаковка tuple та list
+# змінна "_"
+# поняття dict
+# основні методи dict(update(), get(), pop(), keys(), values(), items())
+
+# value_1 = 1
+# value_2 = 2
+#
+# value_1, value_2 = value_2, value_1
+
+
+# value_list = (1, 2, 3, 4, 6, 7, 8)
+
+# value_1, value_2, value_3 = value_list[:3]
+
+# value_1, value_2, value_3, _ = value_list
+
+# _ когда ставят , значит данние больше не будут использовать, зустрічається у фор
+# tmp - тимчасова змінна
+#  *tmp -если не знаем какое количество прилетит
+
+
+
+# request_list = ["", "", ""]
+
