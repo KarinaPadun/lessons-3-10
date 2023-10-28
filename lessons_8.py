6daab605a87a98a62a119b6958cf79a1fb1935f0
karina <kmalina946@gmail.com>
2023-10-28 11:50:43 +0300

    editing lesson 2

--- lessons_8.py
+++ lessons_8.py
@@ -17,9 +17,21 @@
 
 # _ когда ставят , значит данние больше не будут использовать, зустрічається у фор
 # tmp - тимчасова змінна
-#  *tmp -если не знаем какое количество прилетит
+#  *tmp -если не знаем какое количество прилетит , перекладає у лист
 
-
+# print(_)
+# print(value_1, value_2, value_3, tmp)
+# print(value_list)
+# print(*value_list) - лист без скобок, розпаковка
 
 # request_list = ["", "", ""]
+
+
+# if len(request_list) > 3:
+#     value_1, value_2, value_3, *tmp = request_list
+# else:
+#     value_1, value_2, value_3 = request_list
+
+# value_1, value_2, value_3, *tmp = value_list
+
 
