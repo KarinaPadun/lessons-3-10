af53648b43b84beafc2be5d25b4745f41eac1f3b
karina <kmalina946@gmail.com>
2023-10-30 21:48:57 +0200

    editing lesson 2

--- homework_8.py
+++ homework_8.py
@@ -486,3 +486,34 @@ for key in keys_1:
 
 print(my_new_dict)
 
+# 5.2  г) Об'єднати ці два словники у новий словник за правилом:
+
+my_dict_1 = {
+    "name": "John",
+    "pets": "cat",
+    "city": "New York",
+    "work": "office"
+}
+
+my_dict_2 = {
+    "name": "Jonn",
+    "age": 43,
+    "family": "mom and dad",
+    "work": "shop assistant"
+}
+
+my_new_dict = {}
+
+for key in my_dict_1:
+    if key not in my_dict_2:
+        my_new_dict[key] = my_dict_1[key]
+
+for key in my_dict_2:
+    if key not in my_dict_1:
+        my_new_dict[key] = my_dict_2[key]
+
+for key in my_dict_1:
+    if key in my_dict_2:
+        my_new_dict[key] = [my_dict_1[key], my_dict_2[key]]
+print(my_new_dict)
+
--- lesson_1.py
+++ lesson_1.py
@@ -1,5 +1,38 @@
 # print("hello world")
 
+# 5.2  г) Об'єднати ці два словники у новий словник за правилом:
+
+my_dict_1 = {
+    "name": "John",
+    "pets": "cat",
+    "city": "New York",
+    "work": "office"
+}
+
+my_dict_2 = {
+    "name": "Jonn",
+    "age": 43,
+    "family": "mom and dad",
+    "work": "shop assistant"
+}
+
+my_new_dict = {}
+
+for key in my_dict_1: # ключ в моем словаре один
+    if key not in my_dict_2: # если ключа нет в моем словаре 2
+        my_new_dict[key] = my_dict_1[key] # добавляю в мой новий словарь ключ + значение
+
+for key in my_dict_2: # ключ в моем словаре два
+    if key not in my_dict_1: # если ключа нет в моем словаре 1
+        my_new_dict[key] = my_dict_2[key] # добавляю в мой новий словарь ключ + значение
+
+for key in my_dict_1: # ключ в моем словаре один
+    if key in my_dict_2: # если ключ есть в моем словаре 2
+        my_new_dict[key] = [my_dict_1[key], my_dict_2[key]] # добавляю в мой новий словарь ключ + значение из словаря один+значение из словаря два
+
+print(my_new_dict)
+
+# первій вариант
 my_dict_1 = [
     {"name": "John"},
     {"pets": "cat"},
@@ -16,15 +49,16 @@ my_dict_2 = [
 
 my_new_dict = {}
 
-# Запитуємо всі унікальні ключі з обох словників
-keys_1 = {list(i.keys())[0] for i in my_dict_1}
-keys_2 = {list(i.keys())[0] for i in my_dict_2}
+keys_1 = {list(i.keys())[0] for i in my_dict_1} # собираю в лист ключи из словаря один
+keys_2 = {list(i.keys())[0] for i in my_dict_2} # собираю в лист ключи из словаря два
 
-# Перевіряємо ключі і об'єднуємо словники
-for key in keys_1:
-    if key in keys_2:
+for key in keys_1: # ключ из ключей один
+    if key in keys_2: # если ключ есть в ключах два
         my_new_dict[key] = [i[key] for i in my_dict_1 if key in i] + [i[key] for i in my_dict_2 if key in i]
-    else:
-        my_new_dict[key] = [i[key] for i in my_dict_1 if key in i][0]
+    # мой новий словарь(ключ)  = переменная ключа из словаря один если ключ есть в переменной
+    # плюс переменная ключа из словаря два если ключ есть в переменной
+    else: # в другом случае
+        my_new_dict[key] = [i[key] for i in my_dict_1 if key in i][0] # мой новий словарь ключ = переменная ключа из словаря 1
+        # если ключ есть в переменной
 
 print(my_new_dict)
