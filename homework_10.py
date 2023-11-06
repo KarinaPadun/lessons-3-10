e4aefc83d408681e53cd74c8d20edf2025d98678
karina <kmalina946@gmail.com>
2023-11-06 21:23:02 +0200

    editing lesson 2

--- homework_10.py
+++ homework_10.py
@@ -119,8 +119,3 @@ filename = 'hom_1_3.py'
 result = dates_from_file(filename)
 print(result)
 
-# Написати функцію, яка отримує у вигляді параметра ім'я файлу (authors.txt) та повертає список
-# словників виду {"date_original": date_original, "date_modified": date_modified}
-# у яких date_original - це дата з рядка (якщо є),
-# а date_modified - ця ж дата, представлена у форматі "dd/mm/yyyy" (d-день, m-місяць, y-рік)
-# Наприклад [{"date_original": "8th February 1828", "date_modified": 08/02/1828}, ...]
