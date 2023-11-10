617304ad31baf2f4e9b3ddcb634bf6f826e40121
karina <kmalina946@gmail.com>
2023-11-10 19:13:25 +0200

    editing lesson 2

--- test_G.py
+++ test_G.py
@@ -1,41 +1,16 @@
 import os
 
 
-def names_of_directories(my_directory_path) -> dict:
-    if not os.path.exists(my_directory_path) or not os.path.isdir(my_directory_path):
-        return {"file_names": [], "dir_names": []}
-
-    items = os.listdir(my_directory_path)
-    full_path = os.path.abspath(my_directory_path)
-
-    file_names = [os.path.join(full_path, i) for i in items if os.path.isfile(os.path.join(full_path, i))]
-    dir_names = [os.path.join(full_path, i) for i in items if os.path.isdir(os.path.join(full_path, i))]
-
-    return {"file_names": file_names, "dir_names": dir_names}
-
-
 def update_directory_info(directory_info, name):
-    full_path = os.path.abspath(name)
-
-    if os.path.isfile(full_path):
-        directory_info['file_names'].append(full_path)
-    elif os.path.isdir(full_path):
-        directory_info['dir_names'].append(full_path)
+    if os.path.isfile(name):
+        directory_info['file_names'].append(name)
+    elif os.path.isdir(name):
+        directory_info['dir_names'].append(name)
 
     return directory_info
 
-
-my_directory_path = '../lessons-3-10'
-result = names_of_directories(my_directory_path)
-print(result)
-
-
-directory_info = {'file_names': ['file1.txt'], 'dir_names': ['subdir1']}
-
-
-updated_info = update_directory_info(directory_info, 'file2.txt')
-
-
-updated_info = update_directory_info(updated_info, 'subdir2')
+# Приклад використання
+directory_info = {'file_names': ['file1.txt', 'file2.txt'], 'dir_names': ['dir1', 'dir2']}
+updated_info = update_directory_info(directory_info, 'file3.txt')
 
 print(updated_info)
