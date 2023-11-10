4c6d0d974052f249a627c2b66676d16fa434cd5e
karina <kmalina946@gmail.com>
2023-11-10 14:55:06 +0200

    editing lesson 2

--- lessons_11.py
+++ lessons_11.py
@@ -60,17 +60,18 @@ import os
 #     return data
 #
 #
-# def writing_json_file(file: str, data: str) -> list:
+# def writing_json_file(file: str, data: str) -> list: повертає ліст
 #     with open(file, 'w') as f:
 #         # data = f.readlines()
 #         json.dump(data, f) - ЗАПИСУВАТИ ЧЕРЕЗ ДАМП
 #     return data
 
 # json -  ЦЕ ТИП (ФОРМАТ) ЗАПИСУ, А НЕ РОЗШИРЕННЯ
-#
+
+# dump - СКЛАД АБО СХОВИЩЕ
 
 # data = reading_json_readlines("utils/test.json") -те саме что текст в кінці
-# # writing_json_file("utils/test_1.json", [1, 2, 3])
+# # writing_json_file("utils/test_1.json", [1, 2, 3]) -  треба віддавати як стрінгу
 #
 # print(data, type(data))
 
