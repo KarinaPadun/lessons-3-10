602e79b59ed34f4b92e83bb940471feb4957520b
karina <kmalina946@gmail.com>
2023-11-10 21:05:41 +0200

    editing lesson 2

--- test_G.py
+++ test_G.py
@@ -108,3 +108,42 @@ print(updated_info_file)
 updated_info_dir = update_directory_info(updated_info_file, 'new_directory')
 print("\nПісля додавання папки:")
 print(updated_info_dir)
+
+
+
+import os
+
+def names_of_directories(my_directory_path) -> dict:
+    if not os.path.exists(my_directory_path) or not os.path.isdir(my_directory_path):
+        return {"file_names": [], "dir_names": []}
+
+    items = os.listdir(my_directory_path)
+
+    file_names = [i for i in items if os.path.isfile(os.path.join(my_directory_path, i))]
+    dir_names = [i for i in items if os.path.isdir(os.path.join(my_directory_path, i))]
+
+    return {"dir_path": my_directory_path, "file_names": file_names, "dir_names": dir_names}
+
+def update_directory_info(directory_info, item_name):
+    full_path = os.path.abspath(os.path.join(directory_info['dir_path'], item_name))
+
+    if os.path.isfile(full_path):
+        directory_info['file_names'].append(item_name)
+    elif os.path.isdir(full_path):
+        directory_info['dir_names'].append(item_name)
+
+    return directory_info
+
+# Приклад використання
+my_directory_path = '../lessons-3-10'
+directory_info = names_of_directories(my_directory_path)
+print("До оновлення:")
+print(directory_info)
+
+# Оновлюємо інформацію про директорію, додаючи новий файл
+updated_info_file = update_directory_info(directory_info, 'new_file.txt')
+print("\nОновлена інформація (додавши файл):", updated_info_file)
+
+# Оновлюємо інформацію про директорію, додаючи нову папку
+updated_info_dir = update_directory_info(updated_info_file, 'new_directory')
+print("\nОновлена інформація (додавши папку):", updated_info_dir)
