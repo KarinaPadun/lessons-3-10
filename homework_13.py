2bbe39f8eafccd20cbe005c612453358d2feb74a
karina <kmalina946@gmail.com>
2023-11-16 19:50:04 +0200

    editing lesson 2

--- homework_13.py
+++ homework_13.py
@@ -1,9 +1,24 @@
 # Написати клас та реалізувати його методи: (основа – ДЗ № 10)
 
 # 1. Ініціалізація класу з одним параметром – ім'я файлу.
-class File:
-    def __init__(self, name):
-        self.name = name
+class FileName:
+    def domains_file(file: str) -> list:
+        domain_list = []
+        try:
+            with open(file, 'r') as my_file:
+                for line in my_file:
+                    line = line.strip()
+                    if line and not line.startswith('#'):
+                        domain_list.append(line.split('.')[-1])
+
+        except FileNotFoundError:
+            print(f"Файл '{file}' не знайдено")
+
+        return domain_list
+
+    filename = 'hom_1_1.py'
+    result = domains_file(filename)
+    print(result)
 
 
 # 2. Написати метод екземпляра класу, який створює атрибут екземпляра класу
