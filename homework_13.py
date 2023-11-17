f868cf0851f51f65eb638a921c0b9809ab1df300
karina <kmalina946@gmail.com>
2023-11-18 01:24:12 +0200

    editing lesson 2

--- homework_13.py
+++ homework_13.py
@@ -196,3 +196,51 @@ if __name__ == '__main__':
     result = Date("hom_1_3.py")
     result.print_dates()
 
+# 4* (*здавати не обов'язково).
+# Написати метод екземпляра класу, отримує у вигляді параметра ім'я файлу (authors.txt) та повертає список
+# словників виду {"date_original": date_original, "date_modified": date_modified}
+# у яких date_original - це дата з рядка (якщо є),
+# а date_modified - ця ж дата, представлена у форматі "dd/mm/yyyy" (d-день, m-місяць, y-рік)
+# Наприклад [{"date_original": "8th February 1828", "date_modified": 08/02/1828}, ...]
+
+class DateMod:
+    def __init__(self, filename):
+        self.filename = filename
+        self.date_list = self.dates_from_file()
+
+    def process_authors_file(self):
+        date_list = []
+        month_mapping = {
+            "January": "01", "February": "02", "March": "03", "April": "04", "May": "05",
+            "June": "06", "July": "07", "August": "08", "September": "09", "October": "10",
+            "November": "11", "December": "12"
+        }
+        with (((open(self.filename, 'r') as file))):
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                if line.isalpha():
+                    current_month = line
+                elif current_month:
+                        parts = line.split('-')
+                        if len(parts) >= 2:
+                            date_original = parts[0].strip()
+                            date_modified = date_original.replace('st', '').replace('nd', '').replace('rd', '').replace('th', '')
+                            date_list.append({"date_original": date_original, "date_modified": date_modified})
+
+                            return date_list
+
+            def print_dates(self):
+                if self.date_list:
+                    print(self.date_list)
+                else:
+                    print("Список порожній.")
+
+
+
+
+
+if __name__ == '__main__':
+    result = DateMod("hom_1_3.py")
+    result.print_dates()
