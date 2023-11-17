777d2b91b2cda13c8cdc18dbf0d3e45c1b3f488d
karina <kmalina946@gmail.com>
2023-11-17 15:37:10 +0200

    editing lesson 2

--- homework_13.py
+++ homework_13.py
@@ -1,24 +1,70 @@
 # Написати клас та реалізувати його методи: (основа – ДЗ № 10)
 
 # 1. Ініціалізація класу з одним параметром – ім'я файлу.
-class FileName:
-    def domains_file(file: str) -> list:
+class DomainList:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.domain_list = self.load_domains()
+
+    def load_domains(self):
         domain_list = []
         try:
-            with open(file, 'r') as my_file:
+            with open(self.file_name, 'r') as my_file:
                 for line in my_file:
                     line = line.strip()
                     if line and not line.startswith('#'):
                         domain_list.append(line.split('.')[-1])
 
         except FileNotFoundError:
-            print(f"Файл '{file}' не знайдено")
+            print(f"Файл '{self.file_name}' не знайдено")
 
         return domain_list
 
-    filename = 'hom_1_1.py'
-    result = domains_file(filename)
-    print(result)
+    def print_domains(self):
+        if self.domain_list:
+            print(self.domain_list)
+        else:
+            print("Список імен порожній.")
+
+
+filename = 'hom_1_1.py'
+domain_instance = DomainList(filename)
+domain_instance.print_domains()
+
+# 1.2
+
+
+class DomainList:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.domain_list = self.load_domains()
+
+    def load_domains(self):
+        domain_list = []
+        try:
+            with open(self.file_name, 'r') as my_file:
+                for line in my_file:
+                    domain = line.strip()
+                    domain = domain.strip('.')
+                    domain_list.append(domain)
+
+        except FileNotFoundError:
+            print(f"Файл '{self.file_name}' не знайдено")
+
+        return domain_list
+
+    def print_domains(self):
+        if self.domain_list:
+            print(self.domain_list)
+        else:
+            print("Список порожній.")
+
+
+
+filename = 'hom_1_1.py'
+domain_instance = DomainList(filename)
+domain_instance.print_domains()
+
 
 
 # 2. Написати метод екземпляра класу, який створює атрибут екземпляра класу
--- test_file.py
+++ test_file.py
@@ -1,12 +1,30 @@
-from collections.abc import Iterable
+class DomainList:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.domain_list = self.load_domains()
 
-list = [1, 2, 3, 4]
+    def load_domains(self):
+        domain_list = []
+        try:
+            with open(self.file_name, 'r') as my_file:
+                for line in my_file:
+                    line = line.strip()
+                    if line and not line.startswith('#'):
+                        domain_list.append(line.split('.')[-1])
+
+        except FileNotFoundError:
+            print(f"Файл '{self.file_name}' не знайдено")
+
+        return domain_list
+
+    def print_domains(self):
+        if self.domain_list:
+            print(self.domain_list)
+        else:
+            print("Список імен порожній.")
 
 
-def replace_first(items: list) -> Iterable:
-    list.pop()
-    return list
-
-print(list)
-
+filename = 'hom_1_1.py'
+domain_instance = DomainList(filename)
+domain_instance.print_domains()
 
