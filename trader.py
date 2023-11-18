e0575ee5550c39436e3d0327e3164625881a7cce
karina <kmalina946@gmail.com>
2023-11-18 19:28:58 +0200

    editing lesson 2

--- trader.py
+++ trader.py
@@ -1,1 +1,14 @@
-основний код
+from argparse import ArgumentParser
+
+args = ArgumentParser()
+
+args.add_argument("RATE", type=str)
+args.add_argument("AVAILABLE", type=str)
+args.add_argument("BUY XXX", type=str)
+args.add_argument("SELL XXX", type=str)
+args.add_argument("BUY ALL", type=str)
+args.add_argument("SELL ALL", type=str)
+args.add_argument("NEXT", type=str)
+args.add_argument("RESTART", type=str)
+
+
