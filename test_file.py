1e513272b1acd9bbdf94f1c792a200056851c056
karina <kmalina946@gmail.com>
2023-11-20 15:05:58 +0200

    editing lesson 2

--- test_file.py
+++ test_file.py
@@ -10,3 +10,18 @@ args.add_argument("BUY ALL", type=str)
 args.add_argument("SELL ALL", type=str)
 args.add_argument("NEXT", type=str)
 args.add_argument("RESTART", type=str)
+
+
+
+import json
+
+def load_config():
+    with open('config.json', 'r') as file:
+        return json.load(file)
+
+# Приклад використання
+config = load_config()
+print("Початковий курс:", config["initial_conditions"]["rate"])
+print("Баланс в гривнях:", config["initial_conditions"]["balance_uah"])
+print("Баланс в доларах:", config["initial_conditions"]["balance_usd"])
+print("Дельта:", config["delta"])
