a827de44b74978e3a55b8384f002426088d5116a
karina <kmalina946@gmail.com>
2023-11-22 23:40:26 +0200

    editing lesson 2

--- course_homework.py
+++ course_homework.py
@@ -1,37 +1,26 @@
 import random
 import json
-import os
 from datetime import datetime
 import argparse
 
 
 class Trader:
-    def init(self, config_path, history_path):
+    def __init__(self, config_path, history_path):
         with open(config_path) as f:
             config = json.load(f)
         self.delta = config["delta"]
-
-        if not os.path.isfile(history_path) or os.path.getsize(history_path) == 0:
-            self.uah_balance = 10000.00
-            self.usd_balance = 0.00
-        else:
-            with open(history_path, "r") as history_file:
-                for line in history_file:
-                    data = json.loads(line)
-                    if data["action"] == "RESTART":
-                        self.uah_balance = data["amount"]
-                        self.usd_balance = 0.00
-                        break
-
+        self.rate = 36.00
+        self.uah_balance = 10000.00
+        self.usd_balance = 0.00
         self.history_path = history_path
         self.history = []
 
-    def save_to_history(self, action, amount):
+    def save_to_history(self, action, currency_amount, uah_amount):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        transaction = {"timestamp": timestamp, "action": action, "amount": amount}
+        transaction = {"timestamp": timestamp, "action": action, "currency_amount": currency_amount,
+                       "uah_amount": uah_amount}
         self.history.append(transaction)
-        with open(self.history_path, "a") as history_file:
-            history_file.write(f"{timestamp} - {action}: {amount}\n")
+
 
     def get_rate(self):
         return round(self.rate, 2)
@@ -40,36 +29,27 @@ class Trader:
         return {"USD": round(self.usd_balance, 2), "UAH": round(self.uah_balance, 2)}
 
     def buy(self, amount):
-        if self.uah_balance < amount:
-            print(f"UNAVAILABLE, REQUIRED BALANCE UAH {amount:.2f}, AVAILABLE {self.uah_balance:.2f}")
+        if self.uah_balance < amount * self.rate:
+            print(f"UNAVAILABLE, REQUIRED BALANCE UAH {amount * self.rate:.2f}, AVAILABLE {self.uah_balance:.2f}")
             return
-        self.uah_balance -= amount
-        self.usd_balance += amount / self.rate
-        self.save_to_history("BUY", amount)
+        self.uah_balance -= amount * self.rate
+        self.usd_balance += amount
+        self.save_to_history("BUY", amount, amount * self.rate)
 
     def sell(self, amount):
-        if amount == "ALL":
-            amount = self.usd_balance
-        else:
-            try:
-                amount = float(amount)
-            except ValueError:
-                print("Invalid amount format")
-                return
-
         if self.usd_balance < amount:
             print(f"UNAVAILABLE, REQUIRED BALANCE USD {amount:.2f}, AVAILABLE {self.usd_balance:.2f}")
             return
         self.usd_balance -= amount
         self.uah_balance += amount / self.rate
-        self.save_to_history("SELL", amount)
+        self.save_to_history("SELL", amount, amount / self.rate)
 
     def buy_all(self):
         if self.uah_balance == 0:
             print(f"UNAVAILABLE, REQUIRED BALANCE UAH {self.uah_balance:.2f}, AVAILABLE 0.00")
             return
         amount = self.uah_balance / self.rate
-        self.buy(amount)
+        self.save_to_history("BUY", amount, amount * self.rate)
 
     def sell_all(self):
         amount = self.usd_balance
@@ -133,3 +113,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
