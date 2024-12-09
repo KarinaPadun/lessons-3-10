a983f1216b2726c0d52001a1f7160610e617f57f
karina <kmalina946@gmail.com>
2024-12-09 23:55:55 +0200

    editing lesson 2

---
+++ main.py
@@ -0,0 +1,15 @@
+import pymysql
+from config1 import host, user, password, db_name
+
+try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("successfully connected...")
+except Exception as ex:
+    print("connection refused...")
