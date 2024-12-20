e0b270837874e46588dd9780ff5ae2e86bd9a0c3
karina <kmalina946@gmail.com>
2024-12-20 22:42:14 +0200

    editing lesson 2

---
+++ test_Padun_K.py
@@ -0,0 +1,28 @@
+from math import pi
+
+def area_and_perimeter(form, num_2, op):
+    if form == 'square': # Квадрат
+        area =
+        perimeter =
+    elif form == 'circle':# круг
+        area =
+        perimeter =
+    elif form == 'triangle': # треугольник
+        area =
+        perimeter =
+    elif form == 'rectangle': # прямугольник
+        area =
+        perimeter =
+
+    else:
+        return "Error: wrong figure"
+    return result
+
+form = input().lower()
+def area_square:
+    """
+    This func
+    :return: square area
+    """
+    return a^2
+
