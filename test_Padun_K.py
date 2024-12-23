325d7697404c4116d1eacf6aaf0c7cddc195289c
karina <kmalina946@gmail.com>
2024-12-23 19:48:41 +0200

    editing lesson 2

--- test_Padun_K.py
+++ test_Padun_K.py
@@ -4,15 +4,15 @@ from math import pi, sqrt
 def area_and_perimeter():
     form = input("Write the form of the figure (square, circle, triangle, rectangle): ").strip().lower()
 
-    if form == 'square':  # Квадрат
+    if form == 'square':
         side = float(input("Write the side of the square: "))
         area = side ** 2
         perimeter = side * 4
-    elif form == 'circle':  # круг
+    elif form == 'circle':
         radius = float(input("Write the radius of the circle: "))
         area = pi * (radius ** 2)
         perimeter = pi * radius * 2
-    elif form == 'triangle':  # треугольник
+    elif form == 'triangle':
         side_1 = float(input("Write the first side of the triangle: "))
         side_2 = float(input("Write the second side of the triangle: "))
         side_3 = float(input("Write the third side of the triangle: "))
@@ -22,7 +22,7 @@ def area_and_perimeter():
             area = sqrt(p * (p - side_1) * (p - side_2) * (p - side_3))
         else:
             return "Error: the sides do not form a valid triangle"
-    elif form == 'rectangle':  # прямугольник
+    elif form == 'rectangle':
         side1 = float(input("Write the first side of the rectangle: "))
         side2 = float(input("Write the second side of the rectangle: "))
         area = side1 * side2
@@ -32,7 +32,14 @@ def area_and_perimeter():
     return f"Area: {area: .2f}, Perimeter: {perimeter: .2f}"
 
 
-result = area_and_perimeter()
-print(result)
+continue_calculating = True
+
+while continue_calculating:
+    result = area_and_perimeter()
+    print(result)
+
+    yes_or_no = input('Continue? (y/n): ').strip().lower()
+    if yes_or_no != 'y':
+        continue_calculating = False
 
 
